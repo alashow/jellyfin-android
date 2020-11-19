8d6a3b8d58231e0d5b4d48c70c8b714ad83c103a
Maxr1998 <max.rumpf1998@gmail.com>
2020-11-19 18:22:07 +0100

    WIP: Album and artist screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,6 +111,7 @@ dependencies {
     // UI
     implementation(Dependencies.UI.constraintLayout)
     implementation(Dependencies.UI.material)
+    implementation(Dependencies.UI.palette)
     implementation(Dependencies.UI.webkitX)
     implementation(Dependencies.UI.exoPlayer)
     implementation(Dependencies.UI.modernAndroidPreferences)
